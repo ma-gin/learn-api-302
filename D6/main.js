70b8b04e6341350b802335bfd2a4b38a7ca76689
Matt Ginter <mattginter@gmail.com>
2022-01-31 23:59:00 +0100

    link css

--- D6/main.js
+++ D6/main.js
@@ -1,6 +1,3 @@
-const mainDiv = document.createElement('div')
-document.body.appendChild(mainDiv)
-
 // fetch("https://jsonplaceholder.typicode.com/users")
 //   .then((response) => response.json())
 //   .then((data) => {
@@ -12,12 +9,16 @@ document.body.appendChild(mainDiv)
 //     });
 //   });
 
-const createEmails = (data) => {
-    console.log("working")
+const generateCard = (data) => {
     data.map((element) => {
-    const listItem = document.createElement("li")
-    listItem.innerText = element.email
-    mainDiv.appendChild(listItem)
+        const cardItem = document.createElement("div")
+        const cardName = document.createElement("h4")
+        const cardEmail = document.createElement("p")
+        cardName.innerText = element.name
+        cardEmail.innerText = element.email
+        cardItem.append(cardName, cardEmail)
+        cardItem.className = "card"
+        mainDiv.appendChild(cardItem)
     });
 }
 
@@ -28,13 +29,17 @@ const getEmails = async () => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
-        const test2 = await response.json()
-        console.log(typeof test2)
-        createEmails(test2)
-        return test2;
+        const data = await response.json()
+        console.log(data)
+        console.log(data[0])
+        generateCard(data)
+        return data;
     } catch (e) {
         console.log(e)
     } 
 }
 
 getEmails()
+const mainDiv = document.createElement("div")
+mainDiv.className = "displayDiv"
+document.body.appendChild(mainDiv)
