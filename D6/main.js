972d8b5199471b5d5293e138346d0d2cb1d60a6b
Matt Ginter <mattginter@gmail.com>
2022-02-01 00:30:45 +0100

    add search

--- D6/main.js
+++ D6/main.js
@@ -1,3 +1,16 @@
+const mainDiv = document.createElement("div")
+const searchInput = document.getElementById("search")
+
+let users = []
+
+searchInput.addEventListener("input", (e) => {
+    const search = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(search) || user.email.toLowerCase().includes(search)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+})
+
 // fetch("https://jsonplaceholder.typicode.com/users")
 //   .then((response) => response.json())
 //   .then((data) => {
@@ -10,36 +23,34 @@
 //   });
 
 const generateCard = (data) => {
-    data.map((element) => {
-        const cardItem = document.createElement("div")
-        const cardName = document.createElement("h4")
-        const cardEmail = document.createElement("p")
-        cardName.innerText = element.name
-        cardEmail.innerText = element.email
-        cardItem.append(cardName, cardEmail)
-        cardItem.className = "card"
-        mainDiv.appendChild(cardItem)
-    });
+  users = data.map((element) => {
+    const cardItem = document.createElement("div")
+    const cardName = document.createElement("h4")
+    const cardEmail = document.createElement("p")
+    cardName.innerText = element.name    
+    cardEmail.innerText = element.email
+    cardItem.append(cardName, cardEmail)
+    cardItem.className = "card"
+    mainDiv.appendChild(cardItem)
+    return {name: element.name, email: element.email, element: cardItem}
+  })
 }
 
 const getEmails = async () => {
-
-    try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users")
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`)
-        }
-        const data = await response.json()
-        console.log(data)
-        console.log(data[0])
-        generateCard(data)
-        return data;
-    } catch (e) {
-        console.log(e)
-    } 
+    mainDiv.className = "displayDiv"
+    document.body.appendChild(mainDiv)
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const data = await response.json()
+    // console.log(data)
+    // console.log(data[0])
+    generateCard(data)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 getEmails()
-const mainDiv = document.createElement("div")
-mainDiv.className = "displayDiv"
-document.body.appendChild(mainDiv)
